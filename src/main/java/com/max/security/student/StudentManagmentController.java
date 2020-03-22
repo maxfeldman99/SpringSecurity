fcdf41549e0d7c6669f7dc820ec22c6b338de93b
MAX FELDMAN <maxfeldman99@gmail.com>
2020-03-22 23:07:58 +0200

    added preAuthorize annotations

--- src/main/java/com/max/security/student/StudentManagmentController.java
+++ src/main/java/com/max/security/student/StudentManagmentController.java
@@ -1,5 +1,7 @@
 package com.max.security.student;
 
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
@@ -16,20 +18,24 @@ public class StudentManagmentController {
     );
 
     @GetMapping
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_ADMINTRAINEE')")
     public List<Student> getAllStudents(){
         return STUDENTS;
     }
     @PostMapping
+    @PreAuthorize("hasAuthority('student:write')")
     public void registerNewStudent(@RequestBody Student student){
         System.out.println(student);
     }
 
     @DeleteMapping(path = "{studentId}")
+    @PreAuthorize("hasAuthority('student:write')")
     public void deleteStudent(@PathVariable("studentId") Integer studentId){
         System.out.println(studentId);
     }
 
     @PutMapping(path = "{studentId}")
+    @PreAuthorize("hasAuthority('student:write')")
     public void updateStudent(@PathVariable("studentId") Integer studentId, @RequestBody Student student){
         System.out.println(String.format("%s %s",studentId,student));
     }
